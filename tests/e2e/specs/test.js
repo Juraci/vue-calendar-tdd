ef921117292e3da5c4dce9b2086b69fbf817b3f9
Juraci Vieira <juraci.vieira@gmail.com>
2020-09-12 17:04:55 -0300

    smoke test in cypress

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,14 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+describe('weekly task calendar', () => {
+  before(() => cy.visit('/'))
+
+  it('displays the days of the week', () => {
+    cy.get('.day.column').should('have.length', 7)
+  })
+
+  it('displays the form with the default day set to Monday', () => {
+    cy.get('#calendar-entry').should('be.visible');
+    cy.get('.calendar-entry-day').should('contain', 'Monday');
   })
 })
