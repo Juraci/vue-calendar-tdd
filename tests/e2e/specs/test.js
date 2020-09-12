addbc596892a4b8bc2dadee2be5db4b5f5f77e20
Juraci Vieira <juraci.vieira@gmail.com>
2020-09-12 17:55:01 -0300

    style

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -4,11 +4,11 @@ describe('weekly task calendar', () => {
   before(() => cy.visit('/'))
 
   it('displays the days of the week', () => {
-    cy.get('.day.column').should('have.length', 7)
-  })
+    cy.get('.day.column').should('have.length', 7);
+  });
 
   it('displays the form with the default day set to Monday', () => {
     cy.get('#calendar-entry').should('be.visible');
     cy.get('.calendar-entry-day').should('contain', 'Monday');
-  })
-})
+  });
+});
