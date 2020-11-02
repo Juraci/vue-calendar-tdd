88ace99ca9d99befe9de3f837b7983f9139fffec
Juraci Vieira <juraci.vieira@gmail.com>
2020-11-02 16:07:25 -0300

    using function instead of arrow function for cypress specs

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,25 +1,27 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("weekly task calendar", () => {
-  before(() => cy.visit("/"));
+describe("weekly task calendar", function() {
+  before(function() {
+    cy.visit("/");
+  });
 
-  it("displays the days of the week", () => {
+  it("displays the days of the week", function() {
     cy.get(".day.column").should("have.length", 7);
   });
 
-  it("displays the form with the default day set to Monday", () => {
+  it("displays the form with the default day set to Monday", function() {
     cy.get("#calendar-entry").should("be.visible");
     cy.get(".calendar-entry-day").should("contain", "Monday");
   });
 
-  it("displays the active day in the form when the day is clicked", () => {
+  it("displays the active day in the form when the day is clicked", function() {
     cy.get(".day.column")
       .eq(2)
       .click();
     cy.get(".calendar-entry-day").should("contain", "Wednesday");
   });
 
-  it("submits a new event to the active day", () => {
+  it("submits a new event to the active day", function() {
     cy.get("#calendar-entry input").type("Learn Vue 3.0");
     cy.get("#calendar-entry .button").click();
 
